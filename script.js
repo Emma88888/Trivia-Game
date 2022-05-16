bb73bd290e497187e07fc3b80648e7eef8949041
Emma <langbert95@gmail.com>
2022-05-16 16:56:16 -0400

    Updated CSS and JS

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 //click start, get axios questions
+let question = document.querySelector(".question")
+
+let answers = document.querySelectorAll(".answer-box")
+
+let timeLeft = 10;
+
+// let timer = document.querySelector(".timer")
+//     setInterval(function(){
+//         console.log("Called!");
+//         setTimeout(timerCount, 10000);
+//             if (timeLeft === 10) {
+//                 console.log(timeLeft--)
+//             } else if (timeLeft === 0) {
+//                  window.alert("Out of time!");
+//             }
+// })
+
+// let timer= setInterval(function() {
+//     if (timeLeft === 10) {
+//         timeLeft -= 1;
+//     }
+// })
+// console.log(timer)
+
+
 
 document.querySelector("#start-button").addEventListener("click", (event) => {
     axios({
@@ -8,7 +33,7 @@ document.querySelector("#start-button").addEventListener("click", (event) => {
     .then((response) => {
         console.log(response.data.results[0].question);
         function responses(questions) {
-            responses.forEach( x => console.log(x));
+            responses.forEach(x => console.log(x));
         } 
     })
     .catch((error) => {
